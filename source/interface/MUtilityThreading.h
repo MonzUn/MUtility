df17b6b639c03aa7a83121ba12ffd192a5022c7a
MonzUn <daniel-bengtsson@live.com>
2017-12-23 11:17:28 +0100

    Added threading utility

---
+++ source/interface/MUtilityThreading.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <thread>
+#include <mutex>
+
+namespace ThreadingUtility
+{
+	void JoinThread( std::thread& thread )
+	{
+		if ( thread.joinable() )
+			thread.join();
+		//else
+			// Logger::Log( "Attempted to join unjoinable thread", "ThreadingUtility", LogSeverity::WARNING_MSG ); TODODB: Log
+	}
+
+	void LockMutexes( std::initializer_list <std::mutex*> mutexes )
+	{
+		for ( auto mutex : mutexes )
+		{
+			mutex->lock();
+		}
+	}
+
+	void UnlockMutexes( std::initializer_list<std::mutex*> mutexes )
+	{
+		for ( auto mutex : mutexes )
+		{
+			mutex->unlock();
+		}
+	}
+}
