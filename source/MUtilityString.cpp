85d0f6e7b99b60e388441b60202f7439b3d0a320
MonzUn <daniel-bengtsson@live.com>
2018-02-05 23:33:44 +0100

    Added function for checking if a string is a number with an excepted character

--- source/MUtilityString.cpp
+++ source/MUtilityString.cpp
@@ -7,3 +7,26 @@ bool MUtilityString::IsStringNumber(const std::string& string)
 	while (it != string.end() && std::isdigit(*it)) ++it;
 	return !string.empty() && it == string.end();
 }
+
+bool MUtilityString::IsStringNumberExcept(const std::string& string, char exception, uint32_t maxExceptions)
+{
+	std::string::const_iterator it = string.begin();
+	uint32_t exceptionCount = 0;
+	while (it != string.end())
+	{
+		if (!std::isdigit(*it))
+		{
+			if (*it == exception)
+			{
+				++exceptionCount;
+				if (exceptionCount > maxExceptions)
+					break;
+			}
+			else
+				break;
+		}
+		++it;
+	}
+
+	return !string.empty() && it == string.end();
+}
--- source/interface/MUtilityString.h
+++ source/interface/MUtilityString.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <string>
+#include <limits>
 
 namespace MUtilityString
 {
 	bool IsStringNumber(const std::string& string);
+	bool IsStringNumberExcept(const std::string& string, char exception, uint32_t maxExceptions = std::numeric_limits<uint32_t>::max());
 }
