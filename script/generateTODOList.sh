e1b48cb93e3e2d5e025b7289d5bee8b6025a9b57
MonzUn <daniel-bengtsson@live.com>
2018-03-06 23:46:48 +0100

    Adjusted GenerateTODOList script to output into the "local" folder

--- script/generateTODOList.sh
+++ script/generateTODOList.sh
@@ -1,3 +1,3 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
-./listLinesContainingString.sh ../source TODO ../TODO.txt
+./listLinesContainingString.sh ../source TODO ../local/TODO.txt
