7f0bca4ae828f77a19aa4173c005c2acc70d95d0
MonzUn <daniel-bengtsson@live.com>
2018-03-31 13:33:09 +0200

    Added support for negative integers in IsStringNumber()

--- source/MUtilityString.cpp
+++ source/MUtilityString.cpp
@@ -3,9 +3,15 @@
 
 bool MUtilityString::IsStringNumber(const std::string& string)
 {
-	std::string::const_iterator it = string.begin();
+	if(string.empty())
+		return false;
+
+	if(string.size() == 1 && !std::isdigit(string[0])) // A minus sign alone is not a a number
+		return false;
+
+	std::string::const_iterator it = string[0] == '-' ? string.begin() + 1 : string.begin(); // Allow the first character to be a minus sign
 	while (it != string.end() && std::isdigit(*it)) ++it;
-	return !string.empty() && it == string.end();
+	return it == string.end();
 }
 
 bool MUtilityString::IsStringNumberExcept(const std::string& string, char exception, uint32_t maxExceptions)
